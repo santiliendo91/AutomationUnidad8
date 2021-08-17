fcfb44f18928603bb7ac214338a94f02dec929b3
JALASOFT\Santiago.Liendo <santi.liendo91@gmail.com>
2021-08-17 15:17:24 -0400

    fix code

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
-var x = 10;
-var y = 30;
+var x = 30;
+var y = 20;
 var sum = (x + y);
-if (x === 50 || y === 50 || sum == 50){
+if (x == 50 || y == 50 || sum === 50){
   console.log(true);
 }else{
   console.log(false);
